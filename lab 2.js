a4f44478fe532911d914081b1efd29f8a3e24203
NickyDevilee <99047338@mydavinci.nl>
2018-09-04 13:00:01 +0200

    lab 2 compleet

---
+++ lab 2.js
@@ -0,0 +1,10 @@
+/*Nicky Devilee
+Bol4 applicatieontwikkelaar*/
+const vragen = 'We gaan u 3 vragen stellen.'; // constante voor de vragen alert
+alert(vragen); // alert met de constante erbij
+var Voornaam = prompt('Voer je voornaam in'); //variabele voor de voornaam
+var Achternaam = prompt('Voer je achternaam in'); //variabele voor de achternaam
+var Leeftijd = prompt('Voer je leeftijd in'); //variabele voor de leeftijd
+document.write('Uw voornaam is: ' + Voornaam + '<br>'); //schrijven van het antwoord op de vraag van de voornaam + een enter
+document.write('Uw achternaam is: ' + Achternaam + '<br>'); //schrijven van het antwoord op de vraag van de achternaam + een enter
+document.write('Uw leeftijd is: ' + Leeftijd); //schrijven van het antwoord op de vraag van de leeftijd
